78aed23aa4bf5210310c2a71dda1bc780b661f4c
Amit Roy <amit.roy@emc.com>
2019-06-24 17:46:10 -0400

    wip

---
+++ src/main/java/com/dell/controlpath/App.java
@@ -0,0 +1,13 @@
+package com.dell.controlpath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
